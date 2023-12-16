1787c137889dc27b917dfd3c9a12da42191ce1dd
Dylan Tokita <dtokita@hawaii.edu>
2023-12-15 19:00:08 -1000

    [feat] Implement draw_random_tile from bag

--- tests/test_bag_of_tiles.py
+++ tests/test_bag_of_tiles.py
@@ -7,15 +7,61 @@ def test_bag_of_tiles_init():
     bot = BagOfTiles()
 
     assert bot.indexed_tiles == {}
+    assert bot.number_of_tiles == 0
 
 
 def test_standard_bot_fill_bag():
     bot = StandardBagOfTiles()
-    total_tiles = 0
 
     for letter in standard_tile_counts:
         assert len(bot.indexed_tiles[letter]) == standard_tile_counts[letter]
 
-        total_tiles += len(bot.indexed_tiles[letter])
+    assert bot.number_of_tiles == 104
 
-    assert total_tiles == 104
+
+def test_draw_random_tile_empty_bag():
+    bot = BagOfTiles()
+
+    # Make sure that the bag is empty
+    assert bot.number_of_tiles == 0
+
+    drawn_tile = bot.draw_random_tile()
+
+    # Make sure nothing was drawn from the bag
+    assert drawn_tile is None
+    assert bot.number_of_tiles == 0
+
+
+def test_draw_random_tile_full_bag():
+    bot = StandardBagOfTiles()
+
+    # Make sure there are tiles in the bag
+    initial_number_of_tiles = bot.number_of_tiles
+    assert initial_number_of_tiles > 0
+
+    drawn_tile = bot.draw_random_tile()
+
+    # Make sure something was drawn and that the number_of_tiles
+    # has been decremented by 1
+    assert drawn_tile is not None
+    assert (initial_number_of_tiles - 1) == bot.number_of_tiles
+
+
+def test_draw_random_tile_all_tiles():
+    bot = StandardBagOfTiles()
+
+    # Grab the inital number of tiles in the bag
+    initial_number_of_tiles = bot.number_of_tiles
+    assert initial_number_of_tiles > 0
+
+    # Draw all of the tiles out of the bag
+    for _ in range(initial_number_of_tiles):
+        drawn_tile = bot.draw_random_tile()
+
+        assert drawn_tile is not None
+
+    # Try to draw another tile
+    drawn_tile = bot.draw_random_tile()
+
+    assert drawn_tile is None
+    assert bot.number_of_tiles == 0
--- utils/bag_of_tiles.py
+++ utils/bag_of_tiles.py
@@ -1,4 +1,6 @@
-from typing import List, Dict
+import random
+
+from typing import List, Dict, Optional
 from collections import defaultdict
 
 from utils.letter_tile import LetterTile
@@ -7,22 +9,44 @@ from utils.constants.standard_tile_counts import standard_tile_counts
 
 class BagOfTiles:
     indexed_tiles: Dict[str, List[LetterTile]] = defaultdict(list)
+    number_of_tiles = 0
 
     def __init__(self):
         self._fill_bag()
 
-    def _fill_bag(self):
-        # To be implented in each bag type
+    def _fill_bag(self) -> None:
+        # To be implented in each bag type, needs to fill indexed_tiles
+        # and set the number_of_tiles correctly
         pass
+
+    def draw_random_tile(self) -> Optional[LetterTile]:
+        # Check if there are any tiles available
+        if self.number_of_tiles < 1:
+            return None
+
+        drawn_tile = None
+
+        while drawn_tile is None:
+            # Choose a random letter
+            chosen_letter = random.choice(list(self.indexed_tiles.keys()))
+
+            # Check if there are tiles available for that letter
+            if len(self.indexed_tiles[chosen_letter]):
+                # Remove the letter from the index and decrement the number_of_tiles
+                drawn_tile = self.indexed_tiles[chosen_letter].pop()
+                self.number_of_tiles -= 1
+
+                return drawn_tile
 
 
 class StandardBagOfTiles(BagOfTiles):
-    def _fill_bag(self):
+    def _fill_bag(self) -> None:
         for letter in standard_tile_counts:
             quantity = standard_tile_counts[letter]
 
             for _ in range(quantity):
                 self.indexed_tiles[letter].append(LetterTile(letter))
+                self.number_of_tiles += 1
 
 
 class UnlimitedBagOfTiles(BagOfTiles):
